f7a4813d963163c3311627e051a26f742ee5bc5a
Josh Bernstein <joshua.bernstein@outlook.com>
2019-08-16 15:32:32 -0600

    Added gifs :)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Giphy from './Giphy';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Giphy miss={2}/>
       </header>
     </div>
   );
