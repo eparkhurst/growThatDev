24a7bb65493f3340a7bbbc73065020e63800071d
Eli Parkhurst <eli.parkhurst@cakeinsure.com>
2019-08-16 15:06:42 -0600

    f

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ class App extends React.PureComponent {
     super(props);
     this.state = {
       phrases : ['hello', 'good bye', 'other', 'other2'],
-      phraseIndex: 0,
+      phraseIndex: -1,
       misses: 0,
     }
   }
 
-  componentDidMount() {
+  startGame = () => {
+    this.setState({phraseIndex:0});
     this.startInterval();
-  }
+  };
 
   startInterval(){
     this.timer = setInterval(() => {
-      console.log('hit');
-      if(this.state.phrases.length <= this.state.phraseIndex){
+      if( this.state.phraseIndex === this.state.phrases.length -1){
         clearInterval(this.timer)
       }
-      // this.setState({phraseIndex: this.state.phraseIndex + 1, misses: this.state})
+      this.setState({
+        phraseIndex: this.state.phraseIndex + 1,
+        misses: this.state.misses + 1,
+      })
     }, 5000)
   }
 
@@ -36,6 +39,7 @@ class App extends React.PureComponent {
     const {phrases, phraseIndex, misses } = this.state;
     return (
       <div className="App">
+        <button onClick={this.startGame}>Start</button>
         <h3>{phrases[phraseIndex]}</h3>
         <h3>{misses}</h3>
         <Engine currentWord={phrases[phraseIndex]} success={this.phraseMet}></Engine>
